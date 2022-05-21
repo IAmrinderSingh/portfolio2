05314bcf8ba0c1677e799b19645ebc2da8e1ba37
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-05-21 10:49:51 +0530

    Added home section

---
+++ static/sheet.js
@@ -0,0 +1,9 @@
+
+var options = {
+    strings: ['Amrinder Singh', 'Web Developer','Persuing in CSE'],
+    typeSpeed: 100,
+    loop:true,
+  };
+  
+  var typed = new Typed('#typed', options);
+  
