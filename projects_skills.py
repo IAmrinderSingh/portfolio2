3854860bb35a658b607f6d08823fae8f8cdffde8
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-05-21 12:03:42 +0530

    Displaying my image in home page and added library for icons

--- projects_skills.py
+++ projects_skills.py
@@ -26,6 +26,7 @@ skills=[
     },   
 ]
 
+mypic="/static/images/mypic.jpg";
 
 projects=[
     {
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template
-from projects_skills import skills,projects
+from projects_skills import skills,projects,mypic
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html",len_skills=len(skills),skills=skills,len_pro=len(projects),projects=projects)
+    return render_template("index.html",len_skills=len(skills),skills=skills,len_pro=len(projects),projects=projects,mypic=mypic)
 
 if __name__ == '__main__':
     app.run(debug=True)
