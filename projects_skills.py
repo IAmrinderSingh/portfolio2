6c4aa98bd6d989056e6d16133425467e82c05b84
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-05-21 11:07:34 +0530

    Skill section added

---
+++ projects_skills.py
@@ -0,0 +1,27 @@
+skills=[
+    {
+        "title":"HTML",
+        "icons":"fa-brands fa-html5",
+    },
+    {
+        "title":"CSS",
+        "icons":"fa-brands fa-css3"
+    },
+    {
+        "title":"Javascript",
+        "icons":"fa-brands fa-js-square"
+
+    },
+    {
+        "title":"C++",
+        "icons":"fa-solid fa-square-c"
+    },
+    {
+        "title":"Python",
+        "icons":"fa-brands fa-python"
+    },
+    {
+        "title":"Linux",
+        "icons":"fa-brands fa-linux"
+    },   
+]
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template
-
+from projects_skills import skills
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html",len_skills=len(skills),skills=skills)
 
 if __name__ == '__main__':
     app.run(debug=True)
